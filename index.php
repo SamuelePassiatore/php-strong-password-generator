fdaf17ce6292b2abe6f0041789a3b59d693f9a70
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-08 15:59:41 +0100

    added arrays and used array merge

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 $user_password_length = $_GET['password-length'] ?? null;
 
+// Array of characters
+$lower_chars = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+$upper_chars = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+$numbers = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+$symbols = array("!", "?", "#", "$", "%", "&", "*", "-", "_");
+
+// Array merging
+$all_chars = array_merge($lower_chars, $upper_chars, $numbers, $symbols);
 ?>
 
 <!DOCTYPE html>
