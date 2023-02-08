d1d36e843208cba7d2a4d584fa3848d43bef7bdf
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-08 18:17:45 +0100

    added head php file and used include

--- includes/head.php
+++ includes/head.php
@@ -1,25 +1,21 @@
-<?php
-session_start();
-$password = $_SESSION['password'];
-?>
-
-<!DOCTYPE html>
-<html lang="it">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
-    <title>Password</title>
+    <title>PHP Strong Password Generator</title>
+    <style>
+        .blue-bg-custom {
+            background-color: #001632;
+        }
+
+        .light-blue-custom {
+            color: #808A98;
+        }
+
+        .alert {
+            background-color: #CFF4FC;
+        }
+    </style>
 </head>
-
-<body class="container pt-3">
-    <p class="fs-3"><strong>Password generata:</strong> <?= $password ?></p>
-    <form action="#" method="POST">
-        <a href="http://localhost/php-strong-password-generator" class="btn btn-danger">Torna indietro</a>
-    </form>
-</body>
-
-</html>
--- index.php
+++ index.php
@@ -24,27 +24,10 @@ if (!empty($user_password_length)) {
 <!DOCTYPE html>
 <html lang="it">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
-    <title>PHP Strong Password Generator</title>
-    <style>
-        .blue-bg-custom {
-            background-color: #001632;
-        }
-
-        .light-blue-custom {
-            color: #808A98;
-        }
-
-        .alert {
-            background-color: #CFF4FC;
-        }
-    </style>
-</head>
+<?php
+//Include head html
+include './includes/head.php'
+?>
 
 <body class="blue-bg-custom">
     <div class="container pt-5">
